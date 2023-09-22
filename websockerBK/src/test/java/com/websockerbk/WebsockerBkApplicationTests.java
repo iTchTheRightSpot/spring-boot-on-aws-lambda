2851e0aa68bf1fd19041c7ef9b82c5ee5ac83adb
emmanuelU17 <emmanueluluabuike@gmail.com>
2023-09-22 12:48:33 -0400

    first commit

---
+++ websockerBK/src/test/java/com/websockerbk/WebsockerBkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websockerbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsockerBkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
